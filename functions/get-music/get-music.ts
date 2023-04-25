2e0a7b10c5d9a9d9b5326dbcc500d86f5ee4c9e1
Flávio Nobre <flavionobredev@gmail.com>
2023-04-25 01:12:56 -0300

    feat: add netlify functions to vue project

---
+++ functions/get-music/get-music.ts
@@ -0,0 +1,19 @@
+import { Handler } from "@netlify/functions";
+import axios from "axios";
+
+export const handler: Handler = async (event, context) => {
+  const { data } = await axios.get(
+    "https://www.letras.mus.br/comunidade-catolica-shalom/1419784/"
+  );
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify({
+      message: `Hello, world!`,
+      data,
+    }),
+  };
+};
