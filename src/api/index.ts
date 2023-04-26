ec6ae69d6ce31609a03d6ca2bc21e161381df3a8
Flávio Nobre <flavionobredev@gmail.com>
2023-04-26 00:33:53 -0300

    feat: add docker

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:9000/.netlify/functions",
+  baseURL: `${import.meta.env.BASE_URL}/.netlify/functions`,
   headers: {
     "Content-Type": "application/json",
   },
