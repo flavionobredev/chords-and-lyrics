b838728b4731f4e34e8ac4ebbe4e97be19a0d13c
Flávio Nobre <flavionobredev@gmail.com>
2023-06-09 20:03:25 -0300

    chore: changes to build

--- src/api/helpers/create-client.ts
+++ src/api/helpers/create-client.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
 export const createClient = (resource: string = "") => {
+  const baseUrl =
+    import.meta.env.NODE_ENV === "production" ? "" : "http://localhost:9000";
+  console.log(process.env);
+
   return axios.create({
-    baseURL: `http://localhost:${
-      import.meta.env.VITE_FUNCTIONS_PORT || 9000
-    }/.netlify/functions/${resource}`,
+    baseURL: `${baseUrl}/.netlify/functions/${resource}`,
     headers: {
       "Content-Type": "application/json",
     },
