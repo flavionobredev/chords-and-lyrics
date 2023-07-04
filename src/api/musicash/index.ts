6c952157eaa7b758945c0a8e07d86020b45b0ce2
Flávio Nobre <flavionobredev@gmail.com>
2023-07-04 00:38:40 -0300

    refactor: change dark theme logic and remove unsed files

--- src/api/musicash/index.ts
+++ src/api/musicash/index.ts
@@ -1,11 +1,12 @@
 import axios from "axios";
-export const createClient = (data?: any) => {
+export const MusicashClient = (data?: any) => {
   return axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
     headers: {
       "Content-Type": "application/json",
     },
+    timeout: 2500,
   });
 };
 
-export type Client = ReturnType<typeof createClient>;
+export type MusicashClient = ReturnType<typeof MusicashClient>;
--- src/components/atoms/buttons/large-menu.vue
+++ src/components/atoms/buttons/large-menu.vue
@@ -13,6 +13,6 @@
   @apply transition-all;
   @apply bg-gradient-to-tr from-cyan-500 to-blue-500 hover:scale-[1.01];
   @apply text-white font-semibold;
-  @apply min-w-[250px] min-h-[140px] py-2 px-4 rounded;
+  @apply min-w-[250px] min-h-[140px] py-2 px-4 rounded-xl;
 }
 </style>
